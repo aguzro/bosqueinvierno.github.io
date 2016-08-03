9de54aa33b146cc5fb15b978f0f0501dfd1b1e95
Allison Guzmán <bosquedeinvierno@outlook.com>
2016-08-03 12:06:18 -0400

    Agregado y comentado código para modal. Al no funcionar, se descartó en favor del modal del framework

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-/*===== Cambiar color de nav =====*/ //http://stackoverflow.com/questions/23706003/changing-nav-bar-color-after-scrolling
+/*===== Cambiar color de nav ===== //http://stackoverflow.com/questions/23706003/changing-nav-bar-color-after-scrolling
 $(document).ready(function(){
       $(window).scroll(function() { // check if scroll event happened
         if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
@@ -9,3 +9,33 @@ $(document).ready(function(){
         }
       });
     });
+*/
+
+
+/* ===== Modal ===== */
+
+// Llama al modal
+//var modal = document.getElementById('myModal');
+
+// Llama al botón que abre el modal
+//var btn = document.getElementById("myBtn");
+
+// Llama al span que cierra el modal
+//var span = document.getElementsByClassName("close")[0];
+
+// Cuando hace click en el botón, se abre el modal 
+//btn.onclick = function() {
+//    modal.style.display = "block";
+//}
+
+// cuando se hace click en el span (x), cierra el modal
+//span.onclick = function() {
+//    modal.style.display = "none";
+//}
+
+// Cuando se hace click en cualquier parte fuera del modal, lo cierra
+//window.onclick = function(event) {
+//    if (event.target == modal) {
+//        modal.style.display = "none";
+//    }
+//}
